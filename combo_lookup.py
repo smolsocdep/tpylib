c35652309fe1c4a1c69ae650bf31be63e8cf3fe9
Andrey Pakhomenkov <pakhomenkov.ap@gmail.com>
2019-05-30 10:55:47 +0300

    lint

--- combo_lookup.py
+++ combo_lookup.py
@@ -4,7 +4,7 @@ Created on 28 мая 2019 г.
 @author: pakhomenkov
 '''
 
-class CComboLookup(object):
+class CComboLookup():
     """ Класс реализует Lookup Combobox """
 
     c_kernel = None
@@ -24,9 +24,10 @@ class CComboLookup(object):
         #*** идентификатор должен быть первым
         self.c_query = p_query
 
+
     def load(self, p_combobox):
         """ Загружает данные в комбобокс """
-        
+
         assert p_combobox is not None, "Assert: [combo_lookup.load]: \
             No <p_combobox> parameter specified!"
 
@@ -36,10 +37,46 @@ class CComboLookup(object):
         l_source_data = l_source_cursor.fetchall()
         #*** Обходим выборку
         if l_source_data:
-            
+
             for l_row in l_source_data:
-                
+
                 p_combobox.addItem(l_row[1])
                 self.c_id_dict[p_combobox.count()-1] = l_row[0]
-            #print(self.c_id_dict)
-        
+
+
+    def find_index_by_id(self, p_id):
+        """ Производит поиск в словаре по заданному ID """
+
+        assert p_id is not None, "Assert: [combo_lookup.find_index_by_id]: \
+            No <p_id> parameter specified!"
+
+        for l_key in self.c_id_dict:
+
+            if self.c_id_dict[l_key] == p_id:
+
+                return l_key
+            return None
+
+
+    def find_id_by_index(self, p_index):
+        """ Возвращает ID по заданному индексу """
+
+        assert p_index is not None, "Assert: [combo_lookup.find_id_by_index]: \
+            No <p_index> parameter specified!"
+
+        return self.c_id_dict[p_index]
+ 
+
+    def load_and_select(self, p_combobox, p_id):
+        """Загружает данные в комбо и выбирает строку с заданным ID """
+
+        assert p_combobox is not None, "Assert: [combo_lookup.load_and_select]: \
+            No <p_combobox> parameter specified!"
+        assert p_id is not None, "Assert: [combo_lookup.load]: \
+            No <p_id> parameter specified!"
+
+        self.load(p_combobox)
+        p_combobox.setCurrentIndex(self.find_index_by_id(p_id))
+
+
+    
