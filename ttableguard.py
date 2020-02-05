c0537dd629feda3dc8bcc19cd594a578aafd76b8
Andrey Pakhomenkov <pakhomenkov.ap@gmail.com>
2020-02-05 11:33:29 +0300

    Переделка ttableguard

--- ttableguard.py
+++ ttableguard.py
@@ -4,6 +4,42 @@
 # полей и соответственно выставляет максимальную длину поля
 # в qLineEdit, чтобы исключить ввод данных, которые не будут сохранены.
 
+# CONTRACT_NUMBER_FIELD_NUMBER = 0
+# SUBJECT_FIELD_NUMBER = 1
+# NOTICE_FIELD_NUMBER = 2
+# REGISTRATION_DATE_FIELD_NUMBER = 3
+# RECORD_DATE_FIELD_NUMBER = 4
+# CONTRACT_TYPE_FIELD_NUMBER = 5
+# ORGANIZATION_FIELD_NUMBER = 6
+# DETAIL_FIELDS = ("fnumber", "fsubject", "fnotice",
+#                  "fregistration_date", "frecord_date", "fcontract_type",
+#                  "forganization")
+
+# c_table_guard = tgrd.CTableGuard(connection, "tbl_details")
+# c_table_guard.set_query_for_insert("select {} \
+#                                          from public.tbl_details \
+#                                          limit 1")
+# c_table_guard.set_field_list(DETAIL_FIELDS)
+# if c_table_guard.prepare():
+#   c_table_guard.init_line_edit(self.qSubjectLineEdit, SUBJECT_FIELD_NUMBER)
+#   c_table_guard.init_date_edit(self.qRegistrationDateEdit)
+#   c_table_guard.init_line_edit(self.qNoticeLineEdit, NOTICE_FIELD_NUMBER)
+#   c_table_guard.init_date_edit(self.qRecordDateEdit)
+
+
+# c_table_guard = tgrd.CTableGuard(connection, "tbl_details")
+# c_table_guard.set_query_for_update("select {} \
+#                                         from public.tbl_details \
+#                                         where id = %(p_id)s;",
+#                                         p_record_id)
+# c_table_guard.set_field_list(DETAIL_FIELDS)
+# if c_table_guard.prepare():
+#   c_table_guard.load_line_edit(self.qSubjectLineEdit, SUBJECT_FIELD_NUMBER)
+#   c_table_guard.load_line_edit(self.qNoticeLineEdit, NOTICE_FIELD_NUMBER)
+#   c_table_guard.load_date_edit(self.qRegistrationDateEdit,
+#                                   REGISTRATION_DATE_FIELD_NUMBER)
+#  c_table_guard.load_date_edit(self.qRecordDateEdit, RECORD_DATE_FIELD_NUMBER)
+
 import datetime
 import psycopg2
 
@@ -61,7 +97,7 @@ class CTableGuard():
         """Производит выборку данных для редактирования/добавления."""
         try:
 
-            self.c_source_cursor = self.c_connection().cursor()
+            self.c_source_cursor = self.c_connection.cursor()
             # *** Получим выборку
             l_fields = ", ".join(self.c_field_list)
             l_query = self.c_source_query.format(l_fields)
@@ -82,12 +118,14 @@ class CTableGuard():
                                                "__query_metadata]:"
                                                "No field list was defined in "
                                                "CTableGuard!")
-
+        # print("****** __query_metadata")
         try:
 
+            # print("*** 0.1 ", self.c_connection)
             # *** Получим курсор
-            l_meta_cursor = self.c_connection().cursor()
+            l_meta_cursor = self.c_connection.cursor()
             # *** Получим выборку
+            # print("*** 0.2 ", l_meta_cursor)
             l_param = dict(p_table_name=self.c_table_name)
             l_query = """select column_name, data_type,
                                 character_maximum_length
@@ -100,20 +138,26 @@ class CTableGuard():
             # *** Получим кол-во строк
             l_rows = len(l_meta_data)
             # *** если выборка не пустая...
+            # print("*** 1 ")
             if l_rows > 0:
 
+                # print("*** 2 ")
                 for l_meta_row in l_meta_data:
 
                     if self.c_field_list.count(l_meta_row[0]):
 
+                        # print("*** 3 ")
                         self.c_field_types[l_meta_row[0]] = l_meta_row[1]
                         self.c_field_widthes[l_meta_row[0]] = l_meta_row[2]
             return True, ""
 
         except psycopg2.Error as ex:  # noqa
 
-            return False, (f"При обращении к базе данных возникла "
-                           "исключительная ситуация: {ex.pgerror}")
+            # print("**** Exception!")
+            # print("ex:", ex)
+            # print("ex.pgerror:", ex.pgerror)
+            return False, (f"!!! При обращении к базе данных возникла "
+                           "исключительная ситуация !!! : {ex}:{ex.pgerror}")
 
     def set_query_for_update(self, p_query, p_id):  # +++
         """Задает запрос для загрузки данных в компоненты."""
@@ -148,7 +192,7 @@ class CTableGuard():
         """Получает данные из БД."""
         # *** Получим метаданные выбранной таблицы
         if self.__query_metadata():
-
+            # print("!!!! prepare")
             # *** Откроем выборку для загрузки в контролы
             return self.__reopen_source_query()
         return False
@@ -195,22 +239,25 @@ class CTableGuard():
 
             p_date_edit.setDate(self.c_source_data[0][p_field_idx])
 
-    def init_line_edit(self, p_line_edit, p_field_idx):
+    def init_line_edit(self, p_field_idx):
         """Очищает контрол и задает макс. длину."""
-        assert p_line_edit is not None, ("Assert: [CTableGuard."
-                                         "init_line_edit]:"
-                                         "No <p_line_edit> parameter "
-                                         "specified!")
         assert p_field_idx is not None, ("Assert: [CTableGuard."
                                          "init_line_edit]:"
                                          "No <p_field_idx> parameter "
                                          "specified!")
-
+        # print("===============================")
+        # print("Field list:")
+        # print(self.c_field_list)
         l_field_name = self.c_field_list[p_field_idx]
+        l_length = -1
+        # print("===============================")
+        # print("Field types:")
+        # print(self.c_field_types)
+
         if self.c_field_types[l_field_name] == "character varying":
 
-            p_line_edit.setText("")
-            p_line_edit.setMaxLength(self.c_field_widthes[l_field_name])
+            l_length = self.c_field_widthes[l_field_name]
+        return l_length
 
     # pylint: disable=no-self-use
     def init_date_edit(self, p_date_edit):
